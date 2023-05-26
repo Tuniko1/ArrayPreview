b85026d4ddd277191b7250adf91f31a655ad581e
Arthur <arthurcaco@gmail.com>
2023-05-25 21:50:41 -0300

    Atividade array 80%

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -1,59 +1,64 @@
 package principal;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		/* ArrayList (índice inicia-se em zero) (10,20,30) -> índice: (0, 1, 2)
-		 
-		int[] numeros = new int[5];
-		int[] numeros2 = {100, 20, 30 ,40, 50};
-		
-		numeros[0] = 10;
-		numeros[1] = 25;
-		numeros[2] = 1;
-		numeros[3] = 99;
-		numeros[4] = 33;
-		System.out.println(numeros [3]);
-		System.out.println(numeros2 [0])*/;
-		
+	/**Faça um programa que receba o nome do aluno e as 4 notas bimestrais desse aluno e mostre a media;
+	 * Faça um programa que receba 50 números, mostre os números pares  contidos em cada posição do vetor
+	 *e a sua posição (indice) do vetor;
+	 *Usando o random faça uma simulação de um sorteio, cadastrando 10 pessoas,
+	 *apenas uma pessoa será a ganhadora para a proxima turma de java do senai;
+	 */
 		Scanner sc = new Scanner(System.in);
+		Random randon = new Random();
 		
-		System.out.println("Quantos nomes deseja salvar na memória? ");
-		int qtd = sc.nextInt();
-		sc.nextLine();
 		
-		String[] nomes = new String [qtd];
+		System.out.println("Digite o nome do Aluno: ");
+		String nome = sc.nextLine();
 		
-		//for usado para inserir os dados
-		for(int contador = 0; contador < nomes.length; contador++) {
-			System.out.println("Insira o nome: ");
-			nomes [contador] = sc.nextLine();
+		double[] notas = new double[4];
+		int[] vetor = new int[5];
+		double somaNotas = 0;
+		int op = randon.nextInt(2);
+		String[] nomes = new String [10];
+		
+		
+		for(int i = 0; i < notas.length; i++) {
+			System.out.println("Insira o valor da " + (i + 1) + "ª nota bimestral: ");	
+			notas [i] = sc.nextDouble();
+			somaNotas += notas[i];		
+		}
+		System.out.println("A média do(a) "+nome+" é: "+somaNotas / notas.length);			
+		System.out.println();
+		
+		for(int j = 0; j < vetor.length; j++) {
+			System.out.println("Insira o "+(j + 1)+"° número: ");
+			int numero = sc.nextInt();
+			vetor[j] = numero;
+			
+		}
+						
+		for (int j = 0; j < vetor.length; j++) {
+		if (vetor[j] % 2 == 0 ){
+			System.out.print(vetor[j]+" é Par! índice --> "+j+" ");
+			System.out.println();
+			}		
+		}
+		for(int k = 0; k < nomes.length; k++) {
+			System.out.println("Insira o nome do participante do sorteio: ");
+			nomes [k] = sc.nextLine();
+			System.out.println("Insira um número: ");
+			int n1 = sc.nextInt();
+			
+			if(op == n1) {
+				System.out.println("o número: "+op+" foi sorteado e o vencedor é: "+nomes);
 			}
-		
-		//for usado para exibir os dados
-		for (int contador = 0;contador < nomes.length; contador++) {
-			System.out.println(nomes[contador]);
-		
-		//for each usado para exibir os dados percorrendo por todo registro do array
-		}for (String nome : nomes) {
-			System.out.println(nome);
 		}
 		
-		sc.close();
-		/*
-		String verificar = "Arthur";
-		if(nomes [0].equals(verificar)) {
-			System.out.println("É igual");		
-		}
-		
-		String nome1 = new String("Arthur");
-		String nome2 = new String("Arthur");
-		//String nome3 = "Arthur";
-		
-		System.out.println(nome1.equals(nome2));
-		*/
+		sc.close();				
 		
 	}
 }
